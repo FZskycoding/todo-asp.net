d8658c97ba78c9564caedbe483f123d32f2d9912
FZskycoding <fei890708@gmail.com>
2025-05-28 22:34:39 +0800

    結合SSMS資料庫

--- TodoListApp/Controllers/TodoController.cs
+++ TodoListApp/Controllers/TodoController.cs
@@ -1,58 +1,74 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Data;
 using TodoListApp.Models;
 
 namespace TodoListApp.Controllers
 {
     public class TodoController : Controller
     {
-        private static List<TodoItem> todoList = new List<TodoItem>();
-        public IActionResult Index()
+        private readonly TodoContext _context;
+
+        public TodoController(TodoContext context)
         {
-            return View(todoList);
+            _context = context;
         }
 
+        public IActionResult Index()
+        {
+            var items = _context.TodoItems.ToList();// 從資料庫讀取
+            return View(items);
+        }
+
+        // 新增任務
         [HttpPost]
         public IActionResult Add(string title)
         {
             if (!string.IsNullOrWhiteSpace(title)){
                 var newTodo = new TodoItem
                 {
-                    Id = todoList.Count + 1,
                     Title = title,
                     IsDone = false
                 };
-                todoList.Add(newTodo);
-            }
-            return RedirectToAction("Index");
-        }
-        [HttpPost]
-        public IActionResult Delete(int id)
-        {
-            var item = todoList.FirstOrDefault(i => i.Id == id);
-            if (item != null)
-            {
-                todoList.Remove(item);
+
+                _context.TodoItems.Add(newTodo);
+                _context.SaveChanges();
             }
             return RedirectToAction("Index");
         }
 
+        // 刪除任務
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var item = _context.TodoItems.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                _context.TodoItems.Remove(item);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
+        }
+
+        // 進入編輯畫面
         public IActionResult Edit(int id)
         {
-            var item = todoList.FirstOrDefault(t => t.Id == id);
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == id);
             if (item == null)
             {
                 return NotFound();
             }
-            return View(item);// 對應到View內的Edit.cshtml
+            return View(item);// 對應到Views內Todo內的Edit.cshtml
         }
 
+        //提交編輯內容
         [HttpPost]
         public IActionResult Edit(TodoItem updatedItem)
         {
-            var item = todoList.FirstOrDefault(t => t.Id == updatedItem.Id);
+            var item = _context.TodoItems.FirstOrDefault(t => t.Id == updatedItem.Id);
             if(item != null)
             {
                 item.Title = updatedItem.Title;
+                _context.SaveChanges();
             }
             return RedirectToAction("Index");
         }
---
+++ TodoListApp/Data/TodoContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TodoListApp.Models;
+
+namespace TodoListApp.Data
+{
+    public class TodoContext:DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) : base(options) { }
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
