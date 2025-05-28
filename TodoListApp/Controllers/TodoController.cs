9ee37d337f31329eb1f61fed53bbc5bc9f6a4124
FZskycoding <fei890708@gmail.com>
2025-05-28 16:12:37 +0800

    加入專案檔案。

---
+++ TodoListApp/Controllers/TodoController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TodoListApp.Models;
+
+namespace TodoListApp.Controllers
+{
+    public class TodoController : Controller
+    {
+        private static List<TodoItem> todoList = new List<TodoItem>();
+        public IActionResult Index()
+        {
+            return View(todoList);
+        }
+
+        [HttpPost]
+        public IActionResult Add(string title)
+        {
+            if (!string.IsNullOrWhiteSpace(title)){
+                var newTodo = new TodoItem
+                {
+                    Id = todoList.Count + 1,
+                    Title = title,
+                    IsDone = false
+                };
+                todoList.Add(newTodo);
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpPost]
+        public IActionResult Delete(int id)
+        {
+            var item = todoList.FirstOrDefault(i => i.Id == id);
+            if (item != null)
+            {
+                todoList.Remove(item);
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
